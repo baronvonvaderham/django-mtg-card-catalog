2dd4215ac95817d89b629ae3849939a633d3691d
Scott Johnson <baronvonvaderham@gmail.com>
2022-09-07 16:38:01 -0400

    Fix travis database issues

--- card_catalog/settings.py
+++ card_catalog/settings.py
@@ -57,10 +57,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'cardcatalog',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASS', None),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
         'ATOMIC_REQUESTS': False
     },
 }
